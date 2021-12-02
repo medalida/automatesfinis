798c255fc93b775fe2d6c5cfa111124f666d2048
DELL_PC <mohammed.dabouz@etu.univ-amu.fr>
2021-12-02 20:49:50 +0100

    2.1

--- tp2automates.py
+++ tp2automates.py
@@ -8,28 +8,46 @@ Determinises the automaton if it's non deterministic
 
 from typing import Set, List
 from automaton import Automaton, EPSILON, State, error, warn
+from tp1automates import is_deterministic as tp1_is_deterministic
+from tp1automates import recognizes as tp1_recognizes
+
 import sys
 import pdb # for debugging
 
 ##################
 
 def is_deterministic(a:Automaton)->bool:
-  # Copy-paste or import from previous TPs
-  return False
+  
+  return tp1_is_deterministic(a)
   
 ##################
   
 def recognizes(a:Automaton, word:str)->bool:
-  # Copy-paste or import from previous TPs
-  return False
+  if not is_deterministic(a):
+    a = determinise(a)
+  
+  return tp1_recognizes(a)
   
 ##################
 
 def determinise(a:Automaton):
-  #TODO implement!
-  pass
+  a_determinised = Automaton(a.name + '_determinised')
+  for stateIndex in a.statesdict:
+    for letter in a.statesdict[stateIndex].transitions:
+      if '%' == letter:
+        for TransState in list(a.statesdict[stateIndex].transitions['%']):
+          for TransLetter in TransState.transitions:
+            for TransState2 in list(TransState.transitions[TransLetter]):
+              a_determinised.add_transition(stateIndex, TransLetter, TransState2)
+      else:
+        for index in list(a.statesdict[stateIndex].transitions[letter]):
+          a_determinised.add_transition(stateIndex, letter, index.name)
+    if a.statesdict[stateIndex].is_accept:
+      a_determinised.statesdict[stateIndex].make_accept()
+
+  return a_determinised
   
-##################
+################## 
 
 if __name__ == "__main__" :
   if len(sys.argv) != 3:
